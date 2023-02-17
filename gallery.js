e8915b6d83b920974330b3f1deb12a6d915f6ae9
JackNDS <jacknds@hotmail.com>
2023-02-17 15:19:17 +1100

    gallery update

--- gallery.js
+++ gallery.js
@@ -1,27 +1,58 @@
-/* Gallery */
+const images = document.querySelectorAll('.column img');
+const lightbox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightbox-image');
+const close = document.querySelector('.close');
+const prevArrow = document.querySelector('.prev-arrow');
+const nextArrow = document.querySelector('.next-arrow');
+let currentIndex = 0;
 
-const current = document.querySelector('#current');
-const imgs = document.querySelectorAll('.imgs img');
-const opacity = 0.6;
-
-// Set first img opacity
-imgs[0].style.opacity = opacity;
-
-imgs.forEach(img => img.addEventListener('click', imgClick));
-
-function imgClick(e) {
-    // Reset the opacity
-    imgs.forEach(img => (img.style.opacity = 1));
-
-    // Change current image to src of clicked image
-    current.src = e.target.src;
-
-    // Add fade in class
-    current.classList.add('fade-in-img');
-
-    // Remove fade-in class after .5 seconds
-    setTimeout(() => current.classList.remove('fade-in-img'), 500);
-
-    // Change the opacity to opacity var
-    e.target.style.opacity = opacity;
+// Display lightbox
+function displayImage(index) {
+  currentIndex = index;
+  lightboxImage.src = images[index].src;
+  lightbox.style.display = 'block';
 }
+
+// Hide lightbox
+function hideImage() {
+  lightbox.style.display = 'none';
+}
+
+// Navigate to next image
+function nextImage() {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  lightboxImage.src = images[currentIndex].src;
+}
+
+// Navigate to previous image
+function prevImage() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  lightboxImage.src = images[currentIndex].src;
+}
+
+// Event listeners
+images.forEach((image, index) => {
+  image.addEventListener('click', () => {
+    displayImage(index);
+  });
+});
+
+close.addEventListener('click', hideImage);
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    hideImage();
+  } else if (event.key === 'ArrowRight') {
+    nextImage();
+  } else if (event.key === 'ArrowLeft') {
+    prevImage();
+  }
+});
+
+prevArrow.addEventListener('click', prevImage);
+nextArrow.addEventListener('click', nextImage);
